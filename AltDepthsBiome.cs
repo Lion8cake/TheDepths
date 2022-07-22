a21786e71ad0d495cb83f3f2cc6942e770d44db9
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2022-07-22 23:13:58 +0500

    Update AltDepthsBiome.cs

--- AltDepthsBiome.cs
+++ AltDepthsBiome.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using Terraria.ID;
 using Terraria.ModLoader;
 using TheDepths.Tiles;
+using Terraria.GameContent;
+using ReLogic.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TheConfectionRebirth
 {
@@ -14,13 +17,18 @@ namespace TheConfectionRebirth
     {
         public override Color NameColor => new(210, 196, 145);
 
-        public override void SetStaticDefaults()
+        public override Asset<Texture2D>[] AltUnderworldBackgrounds => TextureAssets.Underworld;
+		public override Color AltUnderworldColor => new(210, 196, 145);
+
+		public override void SetStaticDefaults()
         {
             BiomeType = BiomeType.Hell;
-            //BiomeStone = ModContent.TileType<ShaleBlock>();
+            BiomeStone = ModContent.TileType<ShaleBlock>();
             //AltarTile = ModContent.TileType<Gemforge>();
+
             DisplayName.SetDefault("Depths");
             Description.SetDefault("A hot gem filled cave deep below the regular caverns that has liquid mercury and quartz");
+            GenPassName.SetDefault("Creating depths...");
         }
 
         public override string WorldIcon => "";
