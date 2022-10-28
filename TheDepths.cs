e121526fbf66ba01a8edcbe1f64110673be4770f
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2022-10-28 18:51:08 +0500

    raw

--- TheDepths.cs
+++ TheDepths.cs
@@ -13,13 +13,15 @@ namespace TheDepths
 {
     public class TheDepths : Mod
     {
-        public static readonly Asset<Texture2D>[] texture = new Asset<Texture2D>[14];
+        public static Asset<Texture2D>[] texture = new Asset<Texture2D>[14];
         public static Mod mod;
         public static List<int> livingFireBlockList;
 
         public override void Load()
         {
             mod = this;
+            for (int i = 0; i < texture.Length; i++)
+                texture[i] = ModContent.Request<Texture2D>("TheDepths/Backgrounds/DepthsUnderworldBG_" + i);
             livingFireBlockList = new List<int> { 336, 340, 341, 342, 343, 344, ModContent.TileType<LivingFog>() };
             IL.Terraria.Main.DrawUnderworldBackgroudLayer += ILMainDrawUnderworldBackground;
             if (!Main.dedServ)
@@ -31,7 +33,7 @@ namespace TheDepths
         private void ILMainDrawUnderworldBackground(ILContext il)
         {
             ILCursor c = new(il);
-            c.GotoNext(MoveType.After, i => i.MatchStloc(2));
+            /*c.GotoNext(MoveType.After, i => i.MatchStloc(2));
                 c.Index -= 2;
             c.Emit(OpCodes.Ldloc, 1);
             c.Emit(OpCodes.Ldloc, 0);
@@ -41,13 +43,22 @@ namespace TheDepths
                     return texture[index];
                 return orig;
             });
-            c.Emit(OpCodes.Stloc, 1);
-        }
+            c.Emit(OpCodes.Stloc, 1);*/
 
-        public override void PostSetupContent()
-        {
-            for (int i = 0; i < texture.Length; i++)
-                texture[i] = ModContent.Request<Texture2D>("TheDepths/Backgrounds/DepthsUnderworldBG_" + i);
+            int asset = 0, texture = 0;
+            c.GotoNext(i => i.MatchLdloc(out asset),
+                i => i.OpCode == OpCodes.Callvirt,
+                i => i.MatchStloc(out texture));
+
+            c.Emit(OpCodes.Ldloc, asset);
+            c.Emit(OpCodes.Ldloc, 0);
+            c.EmitDelegate<Func<Asset<Texture2D>, int, Asset<Texture2D>>>((asset, index) =>
+            {
+                if (WorldBiomeManager.WorldHell == "TheDepths/AltDepthsBiome")
+                    return TheDepths.texture[index];
+                return asset;
+            });
+            c.Emit(OpCodes.Stloc, asset);
         }
 
         public override void Unload()
