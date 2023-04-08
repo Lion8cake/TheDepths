77bfa8bee7c7dec1f1579f88d9fcebf8c045e7ed
ghasttear1 <23445208+ghasttear1@users.noreply.github.com>
2023-04-08 14:02:41 +1000

    Added basic underworld selection for the depths

---
+++ UI/DepthsGroupOptionButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.Localization;
+
+namespace TheDepths.UI;
+
+public class GroupOptionButton<T> : Terraria.GameContent.UI.Elements.GroupOptionButton<T> {
+    public GroupOptionButton(T option, LocalizedText title, LocalizedText description, Color textColor,
+                             string iconTexturePath, float textSize = 1, float titleAlignmentX = 0.5f,
+                             float titleWidthReduction = 10) : base(option, title, description, textColor,
+        iconTexturePath, textSize, titleAlignmentX, titleWidthReduction) {
+    }
+
+    public GroupOptionButton(T option, LocalizedText title, LocalizedText description, Color textColor,
+                             Asset<Texture2D> iconTexture, float textSize = 1, float titleAlignmentX = 0.5f,
+                             float titleWidthReduction = 10) : this(option, title, description, textColor,
+        (string)null, textSize, titleAlignmentX, titleWidthReduction) {
+        typeof(Terraria.GameContent.UI.Elements.GroupOptionButton<T>)
+            .GetField("_iconTexture", BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(this, iconTexture);
+    }
+}
